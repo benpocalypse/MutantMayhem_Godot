cd129fdd526d35b6a22f17e2b9c0feb0195823e6
Unknown <ben.foote@gmail.com>
2021-06-29 14:03:42 -0400

    Fixed touchscreen Intro skip
    Fixed a bug where you couldn't skip the intro on a touchscreen device, and updated the Readme a bit.

--- Scripts/CutsceneIntro.cs
+++ Scripts/CutsceneIntro.cs
@@ -5,29 +5,26 @@ public class CutsceneIntro : Node2D
 {
 	private Generic2dGame game;
 	
-	// Called when the node enters the scene tree for the first time.
 	public override void _Ready()
 	{
 		game = (Generic2dGame)GetNode("/root/Generic2dGame");
 	}
 	
-	private void _on_AnimationPlayer_animation_finished(String anim_name)
-	{
-		game.GotoScene(Generic2dGame.Scenes.Level1);
-	}
-
-  // Called every frame. 'delta' is the elapsed time since the previous frame.
 	public override void _Process(float delta)
 	{
 		if (Input.IsActionJustPressed("ui_accept")	|| 
 			Input.IsActionJustPressed("ui_left")	||
 			Input.IsActionJustPressed("ui_right")	|| 
 			Input.IsActionJustPressed("ui_select")	|| 
-			Input.IsActionJustPressed("ui_cancel")	||
-			Input.IsActionJustPressed("ui_touch"))
+			Input.IsActionJustPressed("ui_cancel") )
 		{
 			game.GotoScene(Generic2dGame.Scenes.Level1);
 		}
+	}
+	
+	private void _on_AnimationPlayer_animation_finished(String anim_name)
+	{
+		game.GotoScene(Generic2dGame.Scenes.Level1);
 	}
 	
 	public override void _UnhandledInput(InputEvent @event)
@@ -36,5 +33,10 @@ public class CutsceneIntro : Node2D
 		{
 			game.GotoScene(Generic2dGame.Scenes.Level1);
 		}
+	}
+	
+	private void _on_TouchScreenButton_pressed()
+	{
+		game.GotoScene(Generic2dGame.Scenes.Level1);
 	}
 }
